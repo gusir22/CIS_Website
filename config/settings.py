97989470c457889dc8a75be15f6d2a8f7a134ac5
Gustavo <gusir22@gmail.com>
2022-11-27 17:19:22 -0500

    updated ALLOWED_HOSTS setting

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,13 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
-ALLOWED_HOSTS = ['cis-staging.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'cis-staging.herokuapp.com',
+    'constructionintegratedservices.herokuapp.com',
+    'constructionintegratedservices.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('sleasyadmin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),  # User management
     path('', include('pages.urls')),  # Pages App
 ] + static(
